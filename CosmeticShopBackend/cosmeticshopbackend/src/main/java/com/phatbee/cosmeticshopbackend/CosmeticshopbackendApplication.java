240dd6b0dc0a00a8e2e378fa0bce748a6b588316
PhatBee <vinhphatst1235@gmail.com>
2025-05-14 20:42:04 +0700

    InitCommit

---
+++ CosmeticShopBackend/cosmeticshopbackend/src/main/java/com/phatbee/cosmeticshopbackend/CosmeticshopbackendApplication.java
@@ -0,0 +1,13 @@
+package com.phatbee.cosmeticshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticshopbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmeticshopbackendApplication.class, args);
+    }
+
+}
---
+++ CosmeticShopBackend/cosmeticshopbackend/src/test/java/com/phatbee/cosmeticshopbackend/CosmeticshopbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phatbee.cosmeticshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticshopbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
